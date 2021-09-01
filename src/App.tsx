eda2baa9f9a874b02cbc1e905358dc118f7254c6
Leonard Choo <leochoodev@gmail.com>
2021-09-01 16:31:48 +0900

    button set up but nodemailer does not work on browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "@fontsource/roboto";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { ButtonGroup, Button, FormControl, TextField } from "@material-ui/core";
-import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
 import SendIcon from "@material-ui/icons/Send";
 
@@ -19,14 +18,50 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+require("dotenv").config({ path: ".env" });
+
+const sendNodemail = (emailField: String) => {
+  var nodemailer = require("nodemailer");
+
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.TEST_USER,
+      pass: process.env.TEST_PASS,
+    },
+  });
+
+  var mailOptions = {
+    from: emailField,
+    to: "leochootest@gmail.com",
+    subject: "React MUI Nodemailer",
+    text: "That was easy!",
+  };
+
+  transporter.sendMail(mailOptions, function (error: any, info: any) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
 function App() {
   const classes = useStyles();
+
+  const [emailField, setEmailField] = useState("Put Your Email");
+
   return (
     <div className={classes.root}>
       <header className="App-header">
-        <TextField defaultValue="Put your email" color="primary"></TextField>
+        <TextField defaultValue={emailField} color="primary"></TextField>
         <ButtonGroup variant="contained" size="large">
-          <Button color="primary" startIcon={<SendIcon />}>
+          <Button
+            onClick={() => sendNodemail(emailField)}
+            color="primary"
+            startIcon={<SendIcon />}
+          >
             SEND
           </Button>
           <Button color="secondary" startIcon={<DeleteIcon />}>
