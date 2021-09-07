6d0a2513d4a52ce9c1bb2380a3ba4fde83e8fbc9
Leonard Choo <leochoodev@gmail.com>
2021-09-07 16:48:35 +0900

    Basic form shaped

--- src/App.tsx
+++ src/App.tsx
@@ -30,14 +30,14 @@ const useStyles = makeStyles((theme: Theme) =>
       },
     },
     input: {
-      color: "#FFFFFF",
+      // color: "#FFFFFF",
     },
     formControl: {
-      margin: theme.spacing(1),
-      width: "100%",
+      // margin: theme.spacing(1),
+      // width: "100%",
     },
     selectEmpty: {
-      marginTop: theme.spacing(2),
+      // marginTop: theme.spacing(2),
     },
   })
 );
@@ -65,6 +65,8 @@ function App() {
   // const [emailField, setEmailField] = useState("Put Your Email");
   const nameInput = useRef<HTMLInputElement>(null);
   const emailInput = useRef<HTMLInputElement>(null);
+  const subjectInput = useRef<HTMLInputElement>(null);
+
   const messageInput = useRef<HTMLInputElement>(null);
 
   const [emailStatus, setEmailStatus] = useState("");
@@ -92,145 +94,110 @@ function App() {
                 Fill up the form and our team will get back to you within 24
                 hours.
               </Typography>
-              <form>
-                <Grid container spacing={1}>
-                  <Grid xs={12} sm={6} md={6} item>
-                    <TextField
-                      placeholder="Enter first name"
-                      label="First Name"
-                      variant="outlined"
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-                  <Grid xs={12} sm={6} md={6} item>
-                    <TextField
-                      placeholder="Enter last name"
-                      label="Last Name"
-                      variant="outlined"
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      type="email"
-                      placeholder="Enter email"
-                      label="Email"
-                      variant="outlined"
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      type="number"
-                      placeholder="Enter phone number"
-                      label="Phone"
-                      variant="outlined"
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      label="Message"
-                      multiline
-                      rows={4}
-                      placeholder="Type your message here"
-                      variant="outlined"
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      color="primary"
-                      fullWidth
+              <Grid container spacing={1}>
+                <Grid xs={12} item>
+                  <FormControl
+                    variant="outlined"
+                    className={classes.formControl}
+                    fullWidth
+                  >
+                    <InputLabel id="sender-inputlabel-id">FROM</InputLabel>
+                    <Select
+                      labelId="sender-inputlabel-id"
+                      id="sender-select-id"
+                      label="sender-select-label"
                     >
-                      Submit
-                    </Button>
-                  </Grid>
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value={10}>sender-1</MenuItem>
+                      <MenuItem value={20}>sender-2</MenuItem>
+                      <MenuItem value={30}>sender-3</MenuItem>
+                    </Select>
+                  </FormControl>
                 </Grid>
-              </form>
+                <Grid xs={12} item>
+                  <FormControl
+                    variant="outlined"
+                    className={classes.formControl}
+                    fullWidth
+                  >
+                    <InputLabel id="template-inputlabel-id">
+                      Template
+                    </InputLabel>
+                    <Select
+                      labelId="template-inputlabel-id"
+                      id="template-select-id"
+                      label="template-select-label"
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value={10}>template-1</MenuItem>
+                      <MenuItem value={20}>template-2</MenuItem>
+                      <MenuItem value={30}>template-3</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid xs={12} sm={6} md={6} item>
+                  <TextField
+                    inputRef={nameInput}
+                    color="primary"
+                    label="Applicant Name"
+                    placeholder="Applicant Name"
+                    InputProps={{
+                      className: classes.input,
+                    }}
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Grid>
+
+                <Grid xs={12} sm={6} md={6} item>
+                  {" "}
+                  <TextField
+                    inputRef={emailInput}
+                    color="primary"
+                    label="Applicant Email"
+                    InputProps={{
+                      className: classes.input,
+                    }}
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Grid>
+                <Grid xs={12} item>
+                  {" "}
+                  <TextField
+                    inputRef={subjectInput}
+                    color="primary"
+                    label="Subject"
+                    InputProps={{
+                      className: classes.input,
+                    }}
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Grid>
+                <Grid xs={12} sm={12} md={12} item>
+                  {" "}
+                  <TextField
+                    inputRef={messageInput}
+                    color="primary"
+                    label="Message"
+                    multiline
+                    rows={14}
+                    InputProps={{
+                      className: classes.input,
+                    }}
+                    variant="outlined"
+                    fullWidth
+                  ></TextField>
+                </Grid>
+              </Grid>
             </CardContent>
           </Card>
         </Grid>
-
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="sender-inputlabel-id">FROM</InputLabel>
-          <Select
-            labelId="sender-inputlabel-id"
-            id="sender-select-id"
-            label="sender-select-label"
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>sender-1</MenuItem>
-            <MenuItem value={20}>sender-2</MenuItem>
-            <MenuItem value={30}>sender-3</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="template-inputlabel-id">Template</InputLabel>
-          <Select
-            labelId="template-inputlabel-id"
-            id="template-select-id"
-            label="template-select-label"
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>template-1</MenuItem>
-            <MenuItem value={20}>template-2</MenuItem>
-            <MenuItem value={30}>template-3</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          inputRef={nameInput}
-          color="primary"
-          label="Applicant Name"
-          placeholder="Name"
-          InputProps={{
-            className: classes.input,
-          }}
-          variant="outlined"
-        ></TextField>
-        <TextField
-          inputRef={nameInput}
-          color="primary"
-          label="UUID"
-          placeholder="UUID"
-          InputProps={{
-            className: classes.input,
-          }}
-          variant="outlined"
-        ></TextField>
-        <TextField
-          inputRef={emailInput}
-          color="primary"
-          label="TO"
-          defaultValue="Email"
-          InputProps={{
-            className: classes.input,
-          }}
-          variant="outlined"
-        ></TextField>
-        <TextField
-          inputRef={messageInput}
-          color="primary"
-          placeholder="Message"
-          multiline
-          rows={14}
-          InputProps={{
-            className: classes.input,
-          }}
-          variant="outlined"
-        ></TextField>
 
         <ButtonGroup variant="contained" size="large">
           <Button
